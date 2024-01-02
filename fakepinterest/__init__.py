e270b9dd30d040e67840b8382df48d2c05c7d3e9
Thiago Regueira <thiago.regueira@yahoo.com.br>
2024-01-02 18:47:35 -0300

    configs para deploy no render

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fakepinterest.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 app.config["SECRET_KEY"] = "super secret key"
 app.config["UPLOAD_FOLDER"] = "fakepinterest/static/fotos_posts"
 
